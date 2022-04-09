f10330d7293672e2a496407d25da8db8e355c2bd
Heidi Mailahn <92544035+hmailahn@users.noreply.github.com>
2022-04-09 18:40:25 -0500

    started adding project data, pushing screenshots to repo so I can add to projects array

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,9 +1,48 @@
-// import React from 'react'
+import React from 'react'
 
-// function Project () {
-//     return(
+function Project () {
 
-//     )
-// }
+const projects= [
+    {
+        image: '',
+        title: 'Outfitters',
+        url: 'https://outfitters-agent-shields.herokuapp.com/login',
+        repository: 'https://github.com/hmailahn/outfitters'
+    },
+    {
+        image: '',
+        title: 'Tech Blog',
+        url: 'https://mighty-journey-90964.herokuapp.com/',
+        repository: 'https://github.com/hmailahn/tech-blog'
+    },
+    {
+        image: '',
+        title: 'WeatheredVibes',
+        url: 'https://sepidehayani.github.io/WeatheredVibes/',
+        repository: 'https://github.com/SepidehAyani/WeatheredVibes'
+    },
+    {
+        image: '',
+        title: 'Photo Port',
+        url: 'https://hmailahn.github.io/photo-port/',
+        repository: 'https://github.com/hmailahn/photo-port'
+    },
+    {
+        image: '',
+        title: 'Budget Tracker',
+        url: 'https://budget-tracker-hmailahn.herokuapp.com/',
+        repository: 'https://github.com/hmailahn/budget-tracker'
+    },
+    {
+        image: '',
+        title: 'Pizza Hunt',
+        url: 'https://pizza-hunt-hmailahn.herokuapp.com/',
+        repository: 'https://github.com/hmailahn/pizza-hunt'
+    }
+]
+    return(
 
-// export default Project
+    )
+}
+
+export default Project
