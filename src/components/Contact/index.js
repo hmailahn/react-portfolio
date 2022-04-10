e3cce0222c9e659ecf98963c008fa48fc3517af8
Heidi Mailahn <92544035+hmailahn@users.noreply.github.com>
2022-04-10 12:48:17 -0500

    added personal info to contact me page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import { validateEmail } from '../../utils/helpers';
 
 
 function ContactForm() {
 
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+
     return (
-        <section>
+        <section className='contact-me'>
           <h1>Contact me</h1>
+          <h3>402-206-9200</h3>
+                <h3>
+                    <a href="mailto:mailahnheidi@gmail.com">mailahnheidi@gmail.com</a>
+                </h3>
+                <h3>
+                    <a href="https://github.com/hmailahn">GitHub</a>
+                </h3>
+                <h3>
+                    <a href="https://www.linkedin.com/in/heidi-mailahn-15005a136/">LinkedIn</a>
+                </h3>
           <form id="contact-form">
             <div>
               <label htmlFor="name">Name:</label>
-              <input type="text" name="name" />
+              <input type="text" defaultValue={name} name="name" />
             </div>
             <div>
               <label htmlFor="email">Email address:</label>
-              <input type="email" name="email" />
+              <input type="email" defaultValue={email} name="email" />
             </div>
             <div>
-              <label htmlFor="message">Message:</label>
+            <textarea name="message" defaultValue={message} rows="5"  />
               <textarea name="message" rows="5" />
             </div>
             <button type="submit">Submit</button>
