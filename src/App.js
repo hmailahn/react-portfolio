9a7cda6e2c4d11837a7eeb2e8b1b2458a58355e4
Heidi Mailahn <92544035+hmailahn@users.noreply.github.com>
2022-04-10 12:38:22 -0500

    added bones of contact form, now going to use state to sync form data"

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import About from './components/About';
 // import Footer from "./components/Footer";
 // import Header from "./components/Header";
-// import Contact from "./components/Contact";
+import ContactForm from "./components/Contact";
 import Nav from "./components/Nav";
 // import Project from "./components/Project";
 
@@ -11,7 +11,8 @@ function App() {
    <div>
 <Nav />
 <main>
-  <About />
+<ContactForm></ContactForm>
+  <About></About>
   {/* <Project /> */}
 </main>
 {/* <Footer /> */}
