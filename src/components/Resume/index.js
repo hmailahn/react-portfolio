31ca1c223815533138a79fccfd7382eab6551012
Heidi Mailahn <92544035+hmailahn@users.noreply.github.com>
2022-05-05 20:44:52 -0500

    updated resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Resume (){
     return (
-        <a href="https://docs.google.com/document/d/1RQh1Q6Zeh9Jy1CCmUge-VypI4Z9cV_L1ZQuftpDbCJY/edit?usp=sharing">
+        <a href="https://docs.google.com/document/d/1myOYJ1MmmkQS3iwxlr0i4SDMGvd4YRunaHWhyRJe5nA/edit?usp=sharing">
 <div id="resume">View my resume here!</div>
 </a>
     )
