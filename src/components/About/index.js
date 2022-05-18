75ea7e9b310e2888eb36a641f940bcc9f3044fdf
Heidi Mailahn <92544035+hmailahn@users.noreply.github.com>
2022-05-17 22:06:47 -0500

    changed about me formatting

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -12,13 +12,26 @@ function About () {
         
         
                 <p className="">
-                    Hi there, I'm Heidi! Minneapolis, MN is where I currently call home with my best friend and two cats! 
-                    I'm a full stack web developer with a background in wellness and a passion for connecting with others. 
+                    Hi there, I'm Heidi! 
+                    <br>
+                    </br>
+                    Minneapolis, MN is where I currently call home with my best friend and two cats! 
+                    <br></br>
+                    I'm a full stack web developer with a background in wellness and a passion for connecting with others.
+                    <br>
+                    </br> 
                     I recently received a full stack certification from the University of Minnesota where I had the opportunity 
                     to learn and enhance my skills on HTML, CSS, JavaScript, the MERN stack, and mySQL. 
+                    <br>
+                    </br>
                     I love creating server-side applications that users find easy, helpful, and fun to use. 
+                    <br>
+                    </br>
                     I'm a valuable team player and love collaborating with others to find out of the box solutions and ideas. 
+                    <br>
+                    </br>
                     I'm self-driven, ambitious, and entergetic about the work I do. 
+                    <br></br>
                     I'd love to work with you and see how we can turn your passions and projects to life!
                 </p>
     
