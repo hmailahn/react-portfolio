85f6f34fa84f868cef4b12297019f8115afdc0c3
Heidi Mailahn <92544035+hmailahn@users.noreply.github.com>
2022-05-23 11:29:58 -0500

    fixed error in resume

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,8 @@
 import React from 'react'
-// import Nav from '../Nav'
+
 
 function Header () {
 
-// const {
-//     pages,
-//     setCurrentPage,
-//     currentPage
-// } = props;
 
     return(
         <header>
@@ -16,35 +11,7 @@ function Header () {
     Heidi Mailahn
     </a>
     </h1>
-{/* <nav>
-    <ul>
-        <li>
-            <a href='#about'>
-            About Me
-            </a>
-        </li>
-        <li>
-            <a href='#contact'>
-           Contact 
-           </a>
-        </li>
-        <li>
-        <a href='#projects'>
-            Projects
-            </a>
-        </li>
-        <li>
-        <a href='#resume'>
-            Resume
-            </a>
-        </li>
-    </ul>
-</nav> */}
-{/* <Nav
-        pages={pages}
-        setCurrentPanel={setCurrentPage}
-        currentPanel={currentPage}
-      /> */}
+
 </header>
     )
 }
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -8,73 +8,50 @@ const [projects] = useState([
         title: 'Weekly Witty Writing Prompts',
         url: 'https://weekly-witty-writing-prompts.herokuapp.com/',
         repository: 'https://github.com/hmailahn/weekly-witty-writing-prompt',
-        description: 'Weekly Witty Writing Prompts is an application where users are presented with a weekly writing prompt and write stories based off that prompt or interact with other user stories.'
+        description: 'Weekly Witty Writing Prompts is an application where users are presented with a weekly writing prompt and write stories based off that prompt or interact with other user stories.',
+        technologies: ''
     },
     {
         title: 'Photo Port',
         url: 'https://hmailahn.github.io/photo-port/',
         repository: 'https://github.com/hmailahn/photo-port',
-        description: "Photo Port is a single page application made from react to showcase a family friend's photography portfolio."
+        description: "Photo Port is a single page application made from react to showcase a family friend's photography portfolio.",
+        technologies: ''
     },
     {
         title: 'Budget Tracker',
         url: 'https://budget-tracker-hmailahn.herokuapp.com/',
         repository: 'https://github.com/hmailahn/budget-tracker',
-        description: 'Budget tracker is an application that allows a user to track withdrawls and deposits with or without data or internet connection.'
+        description: 'Budget tracker is an application that allows a user to track withdrawls and deposits with or without data or internet connection.',
+        technologies: ''
     },
     {
         title: 'Pizza Hunt',
         url: 'https://pizza-hunt-hmailahn.herokuapp.com/',
         repository: 'https://github.com/hmailahn/pizza-hunt',
-        description: 'Pizza hunt is a social media app for pizza lovers to create pizzas and join discussions on other users pizzas.'
+        description: 'Pizza hunt is a social media app for pizza lovers to create pizzas and join discussions on other users pizzas.',
+        technologies: ''
     },
     {
         title: 'Tech Blog',
         url: 'https://mighty-journey-90964.herokuapp.com/',
         repository: 'https://github.com/hmailahn/tech-blog',
-        description: 'Tech Blog is a site where people in the tech community can post and connect with each other over shared blog posts.'
+        description: 'Tech Blog is a site where people in the tech community can post and connect with each other over shared blog posts.',
+        technologies: ''
     },
 
     {
         title: 'WeatheredVibes',
         url: 'https://sepidehayani.github.io/WeatheredVibes/',
         repository: 'https://github.com/SepidehAyani/WeatheredVibes',
-        description: 'WeatheredVibes is an application where users can search for a city and get the current weather and local news based on their search.'
+        description: 'WeatheredVibes is an application where users can search for a city and get the current weather and local news based on their search.',
+        technologies: ''
     },
 
 
 ]);
 
-//=======================================================
-///don't deelte yet
 
-//     return(
-//         <div id="projects">
-//         {projects.map((image, i) => (
-//             <div  className="container">
-//                 <div className="row justify-content-around">
-//                     <div className="col-4">
-//           <a href={image.url}>
-//               <h3>{image.title}</h3>
-              
-//           <img 
-//             src={require(`../../assets/${i}.png`)}
-//             alt={image.title}
-//             key={image.title}
-//             href={image.url}
-//           />
-//           <a className='projectTitle' href={image.repository}>Repository</a>
-//       </a>
-// </div>
-// </div>
-// </div>
-//         ))}
-      
-//       </div>
-//       );
-    
-
-//================================================================
 
     return (
         <section className="container-fluid mb-5">
@@ -82,13 +59,13 @@ const [projects] = useState([
 {projects.map((image, i) => (
 
 <div className="card port-cards col-auto m-4 " style={{ width: '30rem' }}>
+<h3 className="card-title mt-4">{image.title}</h3>
+
 <a href={image.url}>
-  <img src={require(`../../assets/${i}.png`)} className=" port-img " alt={image.title}></img>
+  <img src={require(`../../assets/${i}.png`)} className=" port-img m-2" alt={image.title}></img>
   </a>
-  <div className="card-body">
-    <h5 className="card-title">{image.title}</h5>
-  </div>
-  <div class="card-body">
+
+  <div class="card-body m-2">
     <a href={image.url} class="card-link">Project</a>
     <a href={image.repository} class="card-link">Repository</a>
     <p>{image.description}</p>
